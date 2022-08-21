73c23c7ea085fb20e321294fcb58c56841a83f32
Kishoiyan-Charity1 <kishoiyancharity@gmail.com>
2022-08-21 19:48:36 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,77 @@
+let products= document.getElementById("products")
+let fruits = document.getElementById('fruList')
+let vegetables= document.getElementById('vegList')
+document.getElementById("title").style.color="green";
+
+
+const baseUrl= 'http://localhost:5000';
+
+
+fetch(`${baseUrl}/products/fruits`)
+.then(response=>{
+   response.json()
+   .then(data=>{
+     console.log(data)
+      data.forEach(element => { 
+          fruits.innerHTML += `<li>${element.name}</li>`
+      });
+     
+
+   })
+
+})
+.catch(error=>{
+    console.log(error)
+
+})
+
+fetch(`${baseUrl}/products`)
+.then(response=>{
+   response.json()
+   .then(data=>{
+     console.log(data)
+      data.forEach(element => { 
+          products.innerHTML += `<li>${element.name}</li>`
+      });
+     
+
+   })
+
+})
+.catch(error=>{
+    console.log(error)
+
+})
+
+fetch(`${baseUrl}/products/vegetables`)
+.then(response=>{
+   response.json()
+   .then(data=>{
+     console.log(data)
+      data.forEach(element => { 
+          vegetables.innerHTML += `<li>${element.name}</li>`
+      });
+     
+
+   })
+
+})
+.catch(error=>{
+    console.log(error)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
